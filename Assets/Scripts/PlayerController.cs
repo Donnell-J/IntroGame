7539b704c751d0c8bcd2c36f69a878b81314c8aa
Donnell-J <ec21696@qmul.ac.uk>
2023-10-06 10:27:40 +0100

    PickUp Implementation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,23 +6,24 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour {
     public Vector2 moveValue;
     public float speed;
+    private int count;
 
+    void Start(){
+        count =0;
+    }
     void OnMove(InputValue value){
         moveValue = value.Get<Vector2>();
     }
 
     void FixedUpdate(){
-<<<<<<< HEAD
         Vector3 movement = new Vector3(moveValue.x,0.0f,moveValue.y).normalized;
-=======
-        Vector3 movement=new Vector3(moveValue.x,0.0f,moveValue.y).normalized;
->>>>>>> 03c461cc5580cef0ca6e93798cfa96632bbda49b
         GetComponent<Rigidbody>().AddForce(movement*speed*Time.fixedDeltaTime);
     }
 
     void OnTriggerEnter(Collider other){
         if(other.gameObject.tag == "PickUp"){
             other.gameObject.SetActive(false);
+            count+=1;
         }
     }
 }
